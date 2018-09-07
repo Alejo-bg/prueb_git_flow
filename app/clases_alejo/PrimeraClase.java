36a2cec60a1fe40d4eed7a5d7e4bdd69395b9ede
Alejandro Arboleda <alejandro.arboleda@ceiba.com.co>
2018-09-07 07:57:47 -0500

    GRAL: cree una clase

---
+++ app/clases_alejo/PrimeraClase.java
@@ -0,0 +1,21 @@
+package clases_alejo;
+
+public class PrimeraClase {
+    private String cadena;
+    private Integer numero;
+
+    public PrimeraClase(String cadena, Integer numero) {
+
+        this.cadena = cadena;
+        this.numero = numero;
+    }
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+}
